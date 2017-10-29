daac0882defda9ba49d9e2344d0284b4af8bd3e9
daniela <d.marino10@uniandes.edu.co>
2017-10-29 04:19:29 -0500

    inicio bonito

--- imports/ui/components/Inicio.jsx
+++ imports/ui/components/Inicio.jsx
@@ -14,10 +14,10 @@ class Inicio extends Component{
 		return (
 			<div className="modal">
 				<div className="modalContent">
-			        <h1>¡Bienvenido a the labyrinth!</h1>
-			        <p>Este es un juego de laberinto que funciona con la extension chromevox.</p>
+			        <h1>¡BIENVENIDO A THE LABYRINTH!</h1>
+			        <p>Este es un juego de laberinto que funciona con la extension chromevox</p>
 			        <p>Para iniciar haz click en el siguiente link</p>
-			        <a href="/inicio">Entrar al laberinto</a>
+			        <a href="/inicio"> <img src="./images/seleccion.png" alt=""></img> <span> Entrar al laberinto </span></a>
 			    </div>
 			</div>
 		);
