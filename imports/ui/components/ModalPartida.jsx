477ec1ace20e58c929b9784bbba46d1b43a692e0
hjcalderon10 <hj.calderon10@uniandes.edu.co>
2017-10-30 01:08:30 -0500

    functionality

--- imports/ui/components/ModalPartida.jsx
+++ imports/ui/components/ModalPartida.jsx
@@ -17,9 +17,18 @@ class ModalPartida extends Component{
 	}
 
 	handleChange = (event) => {
-		this.setState({
-			alias: event.target.value,
-		});
+		if(event.target.value ===""){
+    		this.setState({
+				alias: event.target.value,
+    			text:"Por favor ingrese un usuario valido."
+    		});
+    	}
+    	else{
+    		this.setState({
+				alias: event.target.value,
+    			text:""
+    		});	
+    	}
     }
 
 	handleChangeDos = (event) => {
@@ -30,14 +39,7 @@ class ModalPartida extends Component{
 
 
     crearPartida(){
-    	if(this.state.alias !== ""){
-    		this.props.crearPartida(this.state.tipo,this.state.alias);
-    	}
-    	else{
-    		this.setState({
-    			text:"Por favor ingrese un usuario valido."
-    		});
-    	}
+		this.props.crearPartida(this.state.tipo,this.state.alias);
     }	
 
     entrarPartida(){
@@ -52,13 +54,14 @@ class ModalPartida extends Component{
 					<div className="modalContent">
 					    <h1>Crear Partida</h1>
 			            <p className="itemModal">Antes de crear la partida debes seleccionar el tipo de partida y el nombre con el que vas a entrar a ella.</p>
+			            <p>{this.state.text}</p>
 			            <input  className="itemModal" id="alias" aria-label="Nombre en la partida" type="text" placeholder="Nombre" onChange={this.handleChange}></input> 
 			            <select  className="itemModal" aria-label="Tipo de partida" id="tipo" onChange={this.handleChangeDos} defaultValue="coop">		
                             <option value="coop">Cooperativo</option>
                             <option value="vs">Versus</option>
                             <option value="solo">Solo</option>
 				        </select>
-				        <Link 
+				        {this.state.alias!==""? <Link 
 				            className="itemModal" id="boton"
                             aria-label="Crear nueva Partida"
 					        to={{
@@ -66,19 +69,19 @@ class ModalPartida extends Component{
 				            }}
 				            onClick={() => { this.crearPartida()}}>
                         Crear Partida
-			            </Link>                         		
+			            </Link>: null}                         		
                     </div>            
 				:
 					<div className="modalContent">
 			            <p  className="itemModal" >Para entrar a la partida de {this.props.partida.autor} primero escoge un alias</p>
-			            <p>{text}</p>
+			            <p>{this.state.text}</p>
 			            <input  className="itemModal" id="alias" aria-label="Nombre en la partida" type="text" placeholder="Nombre" onChange={this.handleChange}></input> 
-			            <Link  className="itemModal" aria-label="Entrar a partida" to={{
+			            {this.state.alias!==""?<Link  className="itemModal" aria-label="Entrar a partida" to={{
 				            pathname: '/juego'}}
 				            onClick = {() => { this.entrarPartida()}}
 				        >
 	                    Entrar			        
-			            </Link>
+			            </Link>: null}
 			        </div>    
 				}
 			</div>
